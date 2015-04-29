42d0cfc230825bb771a9feffdaa2fbc4b7e40f1f
DeuceTheCoder <ckbeasley@gmail.com>
2015-04-28 22:48:07 -0700

    more llama messages

--- js/main.js
+++ js/main.js
@@ -8,9 +8,17 @@ var shouldLlamaBePraised = function() {
 	if (randomInt % 2 == 0) {
 		llamaPraising = 'oh hell no!'
 	} else if (randomInt % 3 == 0) {
-		llamaPraising = 'all praise the mighty llama!'
+        llamaPraising = bestowWisdom(randomInt);
 	} else if (randomInt % 5 == 0) {
 		llamaPraising = 'PRAISE OR SUFFER THE PRAISING OF LLAMA'
 	}
 	return llamaPraising;
+}
+
+var bestowWisdom = function(wisdom) {
+        if (wisdom % 2 == 0) {
+                    return 'all praise the mighty llama!';
+                        } else {
+                                    return "<div> <img src='llamas/Llama-Doll-icon_orig.png'></img> <span> says \"&#161S&#205! Pero solamente por los justos\"</span> </div>"
+                                            }
 }
